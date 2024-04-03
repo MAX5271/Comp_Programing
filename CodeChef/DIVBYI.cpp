b5ab12248c65394b0164e20b6ee257b46e4bfa38
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-04-04 01:24:53 +0530

    Create DIVBYI.cpp

---
+++ CodeChef/DIVBYI.cpp
@@ -0,0 +1,87 @@
+// LINK TO THE QUESTION : https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/DIVBYI
+// CODE : https://www.codechef.com/viewsolution/1049312523
+
+// template for cp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N=1e5+10;
+long long int arr[N];
+
+
+#define f(n) for (ll int i = 0; i < n; i++)
+#define fn(n) for (ll int i = 1; i <= n; i++)
+
+bool isPrime(ll int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(ll int *a,ll int *b)
+{
+    ll int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll int factorial(ll int n)
+{
+    ll int p = 1;
+    fn(n)
+        p *= i;
+
+    return p;
+}
+
+void solve()
+{
+    ll int n,c=1;
+    cin>>n;
+    
+    ll int a[n];
+    
+    if((n)%2==0)
+    {
+        for(int i=n-2;i>=0;i-=2)
+        a[i]=c++;
+        for(int i=1;i<n;i+=2)
+        a[i]=c++;
+    }
+    else 
+    {
+        for(int i=n-2;i>0;i-=2)
+        a[i]=c++;
+        for(int i=0;i<n;i+=2)
+        a[i]=c++;
+    }
+    
+    for(int i=0;i<n;i++)
+    cout<<a[i]<<" ";
+    cout<<endl;
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
