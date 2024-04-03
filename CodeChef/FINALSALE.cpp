88e7adc5bdc1e4ae787d9e7c9a823d929561d4fc
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-04-04 01:33:08 +0530

    Create FINALSALE.cpp

---
+++ CodeChef/FINALSALE.cpp
@@ -0,0 +1,89 @@
+// LINK TO THE QUESTION : https://www.codechef.com/problems/FINALSALE
+// CODE : https://www.codechef.com/viewsolution/1051912535
+
+// template for cp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N=1e5+10;
+long long int arr[N];
+
+
+#define f(n) for (ll int i = 0; i < n; i++)
+#define fn(n) for (ll int i = 1; i <= n; i++)
+
+bool isPrime(ll int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(ll int *a,ll int *b)
+{
+    ll int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll int factorial(ll int n)
+{
+    ll int p = 1;
+    fn(n)
+        p *= i;
+
+    return p;
+}
+
+void solve()
+{
+    ll int n,num,sum=0,s1=0,m=0,ma=0,index=0;
+    bool end=true;
+    vector<ll int> a;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>num;
+        s1+=num;
+        if(num>m)
+        {
+            index=i;
+            m=num;
+        }
+        
+        a.push_back(num);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        sum+=a[i];
+        ma=max(ma,sum+a[i]);
+    }
+    
+    cout<<ma<<endl;
+   
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
