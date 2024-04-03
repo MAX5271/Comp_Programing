948c6b0c82ce70df8d489bcd1e387986a10465cc
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-04-04 01:26:08 +0530

    Create CHEFNWRK.cpp

---
+++ CodeChef/CHEFNWRK.cpp
@@ -0,0 +1,91 @@
+//LINK TO THE QUESTION : https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/CHEFNWRK
+// CODE : https://www.codechef.com/viewsolution/1049344431
+
+// template for cp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N=1e5+10;
+long long int arr[N];
+
+
+#define f(n) for (ll int i = 0; i < n; i++)
+#define fn(n) for (ll int i = 1; i <= n; i++)
+
+bool isPrime(ll int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(ll int *a,ll int *b)
+{
+    ll int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll int factorial(ll int n)
+{
+    ll int p = 1;
+    fn(n)
+        p *= i;
+
+    return p;
+}
+
+void solve()
+{
+    ll int n,k,sum=0, count = 0;
+    cin>>n>>k;
+    bool flag = true;
+    ll int a[n];
+    
+    for(int i=0;i<n;i++)
+    {
+       cin>>a[i];
+       if (a[i]>k)
+       flag = false;
+    }
+    if(flag)
+    {
+      for(int i=0;i<n;i++)
+      {
+           sum+=a[i];
+           if(sum>k)
+           {
+               count++;
+               sum=a[i];
+           }
+           
+      
+      }
+      
+      cout<<++count<<endl;
+    }
+    else cout<<-1<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
