095fa4e9300cd0b96d9066785f08c53723712d57
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-04-04 01:42:50 +0530

    Create BBWIN.cpp

---
+++ CodeChef/BBWIN.cpp
@@ -0,0 +1,71 @@
+// LINK TO THE QUESTION : https://www.codechef.com/problems/BBWIN
+// CODE : https://www.codechef.com/viewsolution/1053016993
+
+// template for cp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N=1e5+10;
+long long int arr[N];
+
+
+#define f(n) for (ll int i = 0; i < n; i++)
+#define fn(n) for (ll int i = 1; i <= n; i++)
+
+bool isPrime(ll int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(ll int *a,ll int *b)
+{
+    ll int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll int factorial(ll int n)
+{
+    ll int p = 1;
+    fn(n)
+        p *= i;
+
+    return p;
+}
+
+void solve()
+{
+    ll int n,m,p=0;
+    cin>>n>>m;
+    m=m+10;
+   if(n>=m) cout<<0<<endl;
+   else if(m-n==2||m-n==1) cout<<1<<endl;
+   else if((m-n)%3==0) cout<<(m-n)/3<<endl;
+   else cout<<(m-n)/3 +1<<endl;
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
