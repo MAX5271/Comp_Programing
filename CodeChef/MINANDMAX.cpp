257c5aa7d51ee495b7c90b3a7ca2fa6b5543dd74
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-04-04 01:13:01 +0530

    Create MINANDMAX.cpp

---
+++ CodeChef/MINANDMAX.cpp
@@ -0,0 +1,78 @@
+// LINK TO THE QUESTION: https://www.codechef.com/problems/MINANDMAX
+// CODE: https://www.codechef.com/viewsolution/1047733563
+
+// template for cp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N=1e5+10;
+long long int arr[N];
+
+
+#define f(n) for (ll int i = 0; i < n; i++)
+#define fn(n) for (ll int i = 1; i <= n; i++)
+
+bool isPrime(ll int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(ll int *a,ll int *b)
+{
+    ll int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll int factorial(ll int n)
+{
+    ll int p = 1;
+    fn(n)
+        p *= i;
+
+    return p;
+}
+
+void solve()
+{
+    ll int n,q,num=0;
+    cin>>n;
+    bool flag=true;
+    vector<ll int> a,b,c;
+    num=0;
+    if(n%2){
+    num=((n-1)/2)*((n-1)/2+1)+(int)n/2+1;
+    }
+    else
+    num=(n/2)*((n/2)+1);
+    cout<<num<<endl;
+    
+    
+    
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
