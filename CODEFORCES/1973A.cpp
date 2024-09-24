3f572be5a00ddf7d54f9dcbb2abe629c0356755b
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-09-24 20:03:32 +0530

    Add files via upload

---
+++ CODEFORCES/1973A.cpp
@@ -0,0 +1,94 @@
+// template for cp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+#define vec vector<long long int>
+#define set set<long long int>
+const int N=1e5+10;
+long long int arr[N];
+#define all(a) a.begin(),a.end() 
+
+ll int npr(ll int n,ll int r)
+{
+    r=min(r,(n-r));
+    ll int a=1;
+    for(ll int i=0;i<r;i++){
+        a*=(n-i);
+    }
+    for(ll int i=1;i<r;i++){
+        a/=(i+1);
+    }
+    return a;
+}
+
+bool isPrime(ll int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+
+
+constexpr int int_pow(int b, int e, int res = 1)
+{
+    return (e == 0) ? res : int_pow(b, e - 1, b * res);
+}
+
+
+void swap(ll int *a,ll int *b)
+{
+    ll int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll int factorial(ll int n)
+{
+    ll int p = 1;
+    for(ll int i=1;i<=n;i++)
+        p *= i;
+
+    return p;
+}
+
+void solve()
+{
+    ll int a,b,c;
+    cin>>a>>b>>c;
+    if((a+b+c)%2) {cout<<-1<<endl; return;}
+
+    // ll int count=a;
+    // c-=a;
+    // if(b!=0) count+=min(b,c);
+    // cout<<count<<endl;
+
+    if(a+b==c) cout<<c<<endl;
+    else if(a+b>c) cout<<(c+(a+b-c)/2)<<endl;
+    else cout<<a+b<<endl;
+
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
